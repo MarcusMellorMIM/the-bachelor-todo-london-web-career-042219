e05864f464f3eeb74e58341a4f1c2f3729d3998b
Marcus Mellor <marcus@mellor.com>
2019-03-26 09:48:35 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,26 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  current_name=""
+  winner=""
+  return_winner=[]
+  counter=0
+
+  data[season].each do | contestant, contestant_data |
+      
+      if data[season][counter]["status"]=="Winner"
+        winner = data[season][counter]["name"]
+      end        
+      counter+=1
+
+  end
+  
+  MM NOTE --- I HAVE UNDERSTOOD HOW TO USE JSON, BUT NOW NEED TO SIMNPLY RETURN THE FIRST WORD.
+  return_winner = winner.strip
+  
+  puts return_winner
+  return_winner
 end
+
 
 def get_contestant_name(data, occupation)
   # code here
